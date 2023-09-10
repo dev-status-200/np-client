0ced860b1d310e930272bd122b02af193ae34205
dev-status-200 <dev.status.200@gmail.com>
2023-09-10 23:40:52 +0500

    updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import React,{ useState, useEffect } from 'react';
+import React,{ useState } from 'react';
 import Header from '@/Components/Shared/Header/';
 import '@/styles/globals.css';
 import '@/styles/main.scss';
@@ -6,12 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Router, { useRouter } from 'next/router';
 import PortalLayout from '@/Components/Shared/PortalLayout';
 import Loader from '@/Components/Shared/Loader';
+
 export default function App({ Component, pageProps }) {
+
   const [loading, setLoading] = useState(false);
   const router = useRouter();
-  // useEffect(() => {
-  //   console.log(router.pathname)
-  // }, [])
+
+  Router.events.on("routeChangeStart", () => { setLoading(true) });
+  Router.events.on("routeChangeComplete", () => { setLoading(false)});  
 
   return (
   <>
